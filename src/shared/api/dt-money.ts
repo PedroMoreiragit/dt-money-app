6bf135f85213a3626ac25d49ec3ebdca18941769
Pedromoreiragit <pedromoreirat242@gmail.com>
2026-03-09 14:14:38 -0300

    feat: make error class

--- src/shared/api/dt-money.ts
+++ src/shared/api/dt-money.ts
@@ -1,10 +1,22 @@
 import axios from "axios";
 import { Platform } from "react-native";
+import { AppError } from "../helpers/AppError";
 
 const baseURL = Platform.select({
     android: "http://10.0.2.2:3001"
 })
 
 export const dtMoneyApi = axios.create({
-    baseURL, 
+    baseURL,
 })
+
+
+dtMoneyApi.interceptors.response.use(
+    (config) => config,
+    (error) => {
+        if(error.response && error.response.data){
+            return Promise.reject(new AppError(error.response.data.message))
+        }else{
+            return Promise.reject(new AppError("Falha na requisição"));
+        }
+    })
